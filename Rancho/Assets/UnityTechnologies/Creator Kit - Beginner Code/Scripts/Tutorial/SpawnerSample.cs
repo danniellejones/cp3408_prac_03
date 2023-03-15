579ba19a5afbe6833ddbfd692a2bd9403c66de3e
danniellejones <dannielle.jones1@my.jcu.edu.au>
2023-03-15 16:09:07 +1000

    Update completed beginner code

--- Rancho/Assets/UnityTechnologies/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Rancho/Assets/UnityTechnologies/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -7,22 +7,40 @@ public class SpawnerSample : MonoBehaviour
 
     void Start()
     {
-        int angle = 15;
-        Vector3 spawnPosition = transform.position;
+        LootAngle myLootAngle = new LootAngle(45);
+        SpawnHealthPotion(myLootAngle.NextAngle());
+        SpawnHealthPotion(myLootAngle.NextAngle());
+        SpawnHealthPotion(myLootAngle.NextAngle());
+        SpawnHealthPotion(myLootAngle.NextAngle());
+    }
+
+    void SpawnHealthPotion(int angle)
+    {
+        int radius = 5;
 
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
+        Vector3 spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    }
+}
 
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+public class LootAngle
+{
+    int angle;
+    int step;
 
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * 2;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+    public LootAngle(int increment)
+    {
+        step = increment;
+        angle = 0;
+    }
+
+    public int NextAngle()
+    {
+        int currentAngle = angle;
+        angle = Helpers.WrapAngle(angle + step);
+
+        return currentAngle;
     }
 }
 
